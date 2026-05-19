3eba9017d528bdcf84ca56a4be045897342ee953
ayushstwt <ayusrivastava2k3@gmail.com>
2026-05-19 14:22:18 +0530

    pdfbot-ai

---
+++ pdfbot-ai/src/main/java/com/ayshriv/pdfbot/PdfbotAiApplication.java
@@ -0,0 +1,13 @@
+package com.ayshriv.pdfbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfbotAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfbotAiApplication.class, args);
+	}
+
+}
---
+++ pdfbot-ai/src/test/java/com/ayshriv/pdfbot/PdfbotAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayshriv.pdfbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfbotAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
